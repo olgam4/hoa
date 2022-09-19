42e6143b372cf46eb58dd0cb977ee071f098961f
Olivier Gamache <olivier@glo.quebec>
2022-09-19 09:48:16 -0400

    feat: beautiful animal

--- src/islands/companion/index.tsx
+++ src/islands/companion/index.tsx
@@ -13,16 +13,19 @@ const Companion: Component = () => {
 
   return (
     <div class="relative select-none w-[300px] h-[300px]" onclick={add}>
-      <Animation
-        tileset={[cat0, cat1]}
-        refresh={1000}
-      />
+      <div class="absolute bg-black opacity-10 h-[185px] w-[185px] scale-y-50 rounded-full top-[120px] left-[65px]" />
+      <div class="absolute">
+        <Animation
+          tileset={[cat0, cat1]}
+          refresh={1000}
+        />
+      </div>
       <For each={Array.from({ length: quantity() })} children={() => {
-        const [x, y, z] = [Math.random() * 200, Math.random() * -200, Math.random() * 200]
+        const [x, y, z] = [Math.random() * 200, Math.random() * 100, Math.random() * 200]
         return (
           <Motion.div
             class="absolute pointer-events-none"
-            animate={{ y: -900, x: z, transition: { duration: 10 } }}
+            animate={{ y: -900, x: z, opacity: 0, transition: { duration: 10 } }}
             initial={{ y, x }}
           >
             <p class="text-3xl">
@@ -31,7 +34,7 @@ const Companion: Component = () => {
           </Motion.div>
         )
       }} />
-    </div>
+    </div >
   )
 };
 
